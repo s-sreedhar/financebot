6737b26c9a5811b8408b1c338fb91df2788b9ac4
s-sreedhar <s.sreedhargoud@gmail.com>
2024-05-17 18:28:18 +0530

    added api key to the .env file

--- qachat.py
+++ qachat.py
@@ -10,45 +10,25 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-model = genai.GenerativeModel("gemini-pro") 
-chat = model.start_chat(history=[])
+def get_gemini_response(input, prompt):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content([input, prompt])
+    return response.text
 
+input_prompt = """
+You are a finance chatbot with deep knowledge in budgeting, savings, stocks, investments, and other financial matters. 
+Your task is to provide crisp and clear answers to any finance-related questions. If a question is not related to finance, 
+respond with "Please ask something related to finance."
+"""
+st.set_page_config(page_title="AI Application")
+st.header("Finance Bot")
 
-def get_gemini_response(question):
-    if "finance" in question.lower() or "budget" in question.lower() or "investment" in question.lower() or "trade" in question.lower():
-        response = chat.send_message(question, stream=True)
-        return response
-    else:
-        return ["Please ask something related to Finance."]
+input_text=st.text_input("Input:",key="input")
 
-
-st.set_page_config(page_title="Q&A Demo")
-st.header("Finance Chatbot")
-
-
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-
-
-input_text = st.text_input("Input: ", key="input")
-
-
-submit_button = st.button("Ask the question")
-
+submit_button=st.button("Ask the question")
 
 if submit_button and input_text:
-   
-    response = get_gemini_response(input_text)
-    
-    
-    st.session_state['chat_history'].append(("You", input_text))
-    st.subheader("The Response is")
-    
-   
-    for chunk in response:
-        st.write(chunk) 
+    response=get_gemini_response(input_text, input_prompt)
 
-
-st.subheader("The Chat History is")
-for role, text in st.session_state['chat_history']:
-    st.write(f"{role}: {text}")
+    st.subheader("The response is")
+    st.write(response)
